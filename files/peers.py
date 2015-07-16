bc6df242a9128a6092141ec65615061a6be01e57
Shawn Siefkas <shawn.siefkas@meredith.com>
2015-07-15 21:55:04 -0500

    Retry join to ec2 peers

--- files/peers.py
+++ files/peers.py
@@ -14,20 +14,12 @@ def autoscaling_peers():
         for instance in group.instances:
             if instance.instance_id == metadata['instanceId']:
                 group.instances.remove(instance)
-
                 instance_ids = [i.instance_id for i in group.instances]
-                for status in ec2.get_all_instance_status(instance_ids):
-                    if status.instance_status.status != 'ok':
-                        instance_ids.remove(status.id)
-
-                if not instance_ids:
-                    return []
-
                 return ec2.get_only_instances(instance_ids)
 
 if __name__ == '__main__':
     config = {}
-    config['start_join'] = [p.private_dns_name for p in autoscaling_peers()]
+    config['retry_join'] = [p.private_dns_name for p in autoscaling_peers()]
     print json.dumps(config)
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
