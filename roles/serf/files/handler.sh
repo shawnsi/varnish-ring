c5701c2ddc7aad27e3097b39fb62ac61b594192e
Shawn Siefkas <shawn.siefkas@meredith.com>
2015-10-07 16:23:48 -0500

    Updating serf handler

--- roles/serf/files/handler.sh
+++ roles/serf/files/handler.sh
@@ -2,6 +2,16 @@
 
 echo
 echo "New event: ${SERF_EVENT}. Data follows..."
+
 while read line; do
-    printf "${line}\n"
+  host=$(echo $line | awk '{ print $2 }')
+
+  case $SERF_EVENT in
+    member-join)
+      echo curl -X PUT http://localhost:9190/members/${host}:6081
+      ;;
+    member-leave)
+      echo curl -X DELETE http://localhost:9190/members/${host}:6081
+      ;;
+  esac
 done
