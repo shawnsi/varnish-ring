ddd3c9e444386d4af2b58a63eaadf1f025b05756
Shawn Siefkas <shawn.siefkas@meredith.com>
2015-10-08 09:35:15 -0500

    Fix serf peers script

--- roles/cacheproxy/files/peers.sh
+++ roles/cacheproxy/files/peers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for host in $(serf members | awk '$4 == "alive" { print $2 }' | cut -d: -f1 | sort); do
+for host in $(serf members | awk '$3 == "alive" { print $2 }' | cut -d: -f1 | sort); do
     curl -X PUT http://localhost:9190/members/${host}:6081
 done
